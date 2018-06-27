1393919da3886650cbda1661ee48b9e85f041d12
tiffmindset <39908718+tiffmindset@users.noreply.github.com>
2018-06-26 22:42:45 -0400

    Create boolean.c

---
+++ boolean.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main() {
+        int i = 0;
+        printf("i is %d\n", i);
+        if(i) {
+                printf("i is true\n");
+        } else {
+                printf("i is false\n");
+        }
+
+        i = 1;
+        printf("i is %d\n", i);
+        if(i) {
+                printf("i is true\n");
+        } else {
+                printf("i is false\n");
+        }
+
+        i = -1;
+        printf("i is %d\n", i);
+        if(i) {
+                printf("i is true\n");
+        } else {
+                printf("i is false\n");
+        }
+
+        i = (1 > 2);
+        printf("1>2, i is %d\n", i);
+
+        i = (1==1);
+        printf("1==1, i is %d\n", i);
+        return 0;
+}
